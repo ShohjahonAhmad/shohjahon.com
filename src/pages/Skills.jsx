9c830296639253af185bcf8fa9eb85a8e185aa4f
ShohjahonAhmad <shox_0502@mail.ru>
2025-10-16 14:44:34 +0200

    more icons added to skills

--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -1,6 +1,17 @@
 import JavaScriptIcon from '../utils/skills/JavaScript'
 import ReactIcon from '../utils/skills/ReactIcon'
 import TypeScriptIcon from '../utils/skills/TypeScript'
+import ExpressIcon from '../utils/skills/ExpressIcon'
+import GitIcon from '../utils/skills/GitIcon'
+import GitHubIcon from '../utils/skills/GitHubIcon'
+import TailWindCSSIcon from '../utils/skills/TailWindCSSIcon'
+import PostgreSQLIcon from '../utils/skills/PostgreSQLIcon'
+import MongoDBIcon from '../utils/skills/MongoDBIcon'
+import GolangIcon from '../utils/skills/GolangIcon'
+import PythonIcon from '../utils/skills/PythonIcon'
+import DockerIcon from '../utils/skills/DockerIcon'
+import AWSIcon from '../utils/skills/AWSIcon'
+import JavaIcon from '../utils/skills/JavaIcon'
 
 const Skills = () => {
     return (
@@ -11,13 +22,29 @@ const Skills = () => {
             <h1 className="font-extrabold text-5xl text-white mt-6 mb-[100px]">
                 Technologies & Skills
             </h1>
-            <h3 className="text-2xl text-white/50 mb-[40px]">
+            <h3 className="text-2xl text-white/50 mb-10">
                 Techs I use on daily basis
             </h3>
-            <div className="flex gap-8">
+            <div className="flex gap-8 mb-15">
                 <ReactIcon/>
                 <JavaScriptIcon/>
                 <TypeScriptIcon/>
+                <ExpressIcon/>
+                <GitIcon/>
+                <GitHubIcon/>
+                <TailWindCSSIcon/>
+                <PostgreSQLIcon/>
+            </div>
+            <h3 className="text-2xl text-white/50 mb-10">
+                Other techs I've worked with
+            </h3>
+            <div className="flex gap-8">
+                <MongoDBIcon/>
+                <GolangIcon/>
+                <PythonIcon/>
+                <DockerIcon/>
+                <AWSIcon/>
+                <JavaIcon/>
             </div>
         </main>
     )
